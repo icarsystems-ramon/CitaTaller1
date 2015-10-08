ec1f4952cba4950c2ffb77409b3bf1d1d0faea93
icarsystems-ramon <ramon.moliner@icarsystems.es>
2015-10-08 18:14:50 +0200

    Some changes

--- WebAPI/WebAPI/Properties/AssemblyInfo.cs
+++ WebAPI/WebAPI/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CitaTaller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICarSystems")]
 [assembly: AssemblyProduct("CitaTaller")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
