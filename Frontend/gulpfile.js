d6641e0727cef5053a176155a20f227af16c3a9c
icarsystems-ramon <ramon.moliner@icarsystems.es>
2015-10-19 17:17:13 +0200

    Source code del proyecto Frontend. Ember, ember-data, intelisense,e tc

---
+++ Frontend/gulpfile.js
@@ -0,0 +1,27 @@
+﻿var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    bower = require('gulp-bower');
+
+// build
+gulp.task('build:development', shell.task([
+    'ember build --environment development'
+]));
+
+// serve
+gulp.task('build:serve', shell.task([
+    'ember serve'
+]));
+
+// bower
+gulp.task('bower:install', function () {
+    return bower({
+        cmd: 'install'
+    });
+});
+
+gulp.task('bower:update', function () {
+    return bower({
+        cmd: 'update'
+    });
+});
+
