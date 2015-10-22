59cf304a48a7e452a6ec38c4d53949d6fbc69ed9
icarsystems-ramon <ramon.moliner@icarsystems.es>
2015-10-22 18:14:23 +0200

    Cambios en modelo y en peticion en inicializador

--- Frontend/app/models/dms.js
+++ Frontend/app/models/dms.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  domainurl: DS.attr('string'),
+  domainUrl: DS.attr('string'),
   idioma: DS.attr('number'),
   dmsjobs: DS.hasMany('dmsjob'),
   dmstalleres: DS.hasMany('dmstaller'),
