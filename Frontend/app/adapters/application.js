9db9519fc55de4e3319cf960f4526d99aadea27e
Ramon Moliner <ramon.moliner@icarsystems.es>
2015-10-24 02:17:31 +0200

    CORS

--- Frontend/app/adapters/application.js
+++ Frontend/app/adapters/application.js
@@ -5,6 +5,7 @@ export default DS.RESTAdapter.extend({
     host: config.API_HOST,
     namespace: config.API_NAMESPACE,
     // http://stackoverflow.com/questions/29954037/how-to-disable-options-request
+    // http://stackoverflow.com/questions/12013216/how-to-apply-cors-preflight-cache-to-an-entire-domain/12021982#12021982
     // Cambiando el content-type ... reducimos las peticiones OPTIONS y se reduce el tiempo por peticion
     header: {
         "Content-Type": "text/plain"
