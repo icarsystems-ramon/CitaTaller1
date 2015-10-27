1dd78a4768197228b521aa6b0c6f9e8fb4921c96
icarsystems-ramon <ramon.moliner@icarsystems.es>
2015-10-27 17:34:47 +0100

    comment on promise

--- Frontend/app/routes/nuevacita.js
+++ Frontend/app/routes/nuevacita.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     beforeModel: function(){
+        // Ojo! Devuelvo una promise
         return this.store.findAll('dms'); 
         },
 
