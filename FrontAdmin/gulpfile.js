ff4e8d4c6a5dfeeaaa2e693a8e03623679dd3ec3
icarsystems-ramon <ramon.moliner@icarsystems.es>
2015-12-16 10:30:27 +0100

    Creacion proyecto Front-Admin

---
+++ FrontAdmin/gulpfile.js
@@ -0,0 +1,50 @@
+﻿var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    bower = require('gulp-bower');
+
+// build
+gulp.task('ember:build_development', shell.task([
+    'ember build --environment development'
+]));
+
+// build
+gulp.task('ember:build_production', shell.task([
+    'ember build --environment production'
+]));
+
+// build
+gulp.task('ember:build_ios', shell.task([
+    'ember cordova:build --environment production --platform=ios'
+]));
+
+gulp.task('ember:build_android', shell.task([
+    'ember cordova:build --environment production --platform=android'
+]));
+
+gulp.task('ember:build_test', shell.task([
+    'ember build --environment test'
+]));
+
+// serve
+gulp.task('ember:server', shell.task([
+    'ember server'
+]));
+
+// bower
+gulp.task('bower:install', function () {
+    return bower({
+        cmd: 'install'
+    });
+});
+
+gulp.task('bower:update', function () {
+    return bower({
+        cmd: 'update'
+    });
+});
+
+gulp.task('deploy:azure', function () {
+    return bower({
+        cmd: ''
+    });
+});
